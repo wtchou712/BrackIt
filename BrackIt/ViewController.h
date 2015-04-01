9fd43a51b3a14b5ece0762df6eed550377dfea8e
William Chou <wtchou712@gmail.com>
2015-03-31 19:38:15 -0500

    Initial Commit

---
+++ BrackIt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BrackIt
+//
+//  Created by William Chou on 3/31/15.
+//  Copyright (c) 2015 William Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BrackIt/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BrackIt
+//
+//  Created by William Chou on 3/31/15.
+//  Copyright (c) 2015 William Chou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
